f210864a02ad44a9cb4871e3eb629dd3f463074f
Rodney Brooks <assistance90@live.com>
2021-07-05 14:22:52 -0700

    latest

---
+++ AppsDesktop/Business/Data/Data.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppsDesktop
+{
+
+    public class AppsData
+    {
+        public string AppsDBPath { get; set; }
+        public LiteDB.LiteDatabase AppsDB { get; set; }
+    }
+
+    public class AppsContext : DbContext
+    {
+        public AppsContext() : base()
+        {
+
+        }
+
+        public AppsContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=MyDatabase.db");
+            //optionsBuilder..UseInMemoryDatabase("Company");
+            //optionsBuilder.UseNpgsql()
+        }
+
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
